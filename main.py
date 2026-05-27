5941ca38d8ac7b0acb8e8b848f3807de20834833
Igorbtw <igorrodrigues301107@gmail.com>
2026-05-27 11:51:56 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame, sys
-from pygame.locals import QUIT
+from pygame.locals import QUIT, KEYDOWN
 
 clock = pygame.time.Clock()
 
@@ -15,11 +15,10 @@ hero_walk_list = []
 for i in range(4):
     hero_walk_list.append(pygame.image.load(f"assets/assets/Hero_Walk_0{i+1}.png"))
 
-
-curr_framemm = 0
-an
-
-
+run_animacion = False
+curr_frame_mm = 0
+anim_time_mm = 0
+herowalk_spritesheet = pygame.image.load('megaman.png')
 
 pygame.init()
 pygame.display.set_caption("Hello World!")
@@ -27,8 +26,8 @@ pygame.display.set_caption("Hello World!")
 
 
 
-altura = 1200
-largura = 1000
+altura = 170
+largura = 1280
 screen = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption("Hello World!")
 
@@ -37,24 +36,31 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+        
+        
         if event.type == KEYDOWN:
             if event.key == pygame.K_SPACE:
-                run.animation = True
+                run_animacion = True
+    
+    
+    
     clock.tick(60)
     dt = clock.get_time()
     
     #segundos da animação
-    anim_time = anim_time = dt
-    anim_time_sec = anim_time/1000
+    if run_animacion:
+    
+        anim_time = anim_time = dt
+        anim_time_sec = anim_time/1000
 
-    if anim_time_sec > 0.15:
-        curr_frame += 1
-    if curr_frame> len(hero_walk_list) -1:
-       curr_frame = 0
-    anim_time_sec = 0
+        if anim_time_sec > 0.15:
+            curr_frame += 1
+        if curr_frame> len(hero_walk_list) -1:
+            curr_frame = 0
+        anim_time_sec = 0
     #curr_frame += 1
 
-    if run_animation:
+    if run_animacion:
         anim_time_mm
     #Desenho dos elementos na tela
     screen.fill((255,255,255))
@@ -64,5 +70,8 @@ while True:
 
 
     if curr_frame_mm < 5:
-        screen.blit
+        screen.blit(herowalk_spritesheet(200, 200), (60*curr_frame_mm, 60, 60))
+    else:
+        screen.blit(herowalk_spritesheet(200, 200), (60*curr_frame_mm- 5,), 60, 60, 60)
+    
     pygame.display.update()
